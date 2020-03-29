a4d1e2b1b4db02ec217d09163965c81112e1671b
Quentin Manfroi <quentin.manfroi@fr.clara.net>
2020-03-29 17:20:06 +0200

    test

--- main.tf
+++ main.tf
@@ -7,9 +7,25 @@ provider "signalfx" {
   api_url    = "https://api.eu0.signalfx.com"
 }
 
-module "consul_servers" {
+module "cloud-aws" {
   # When using these modules in your own templates, you will need to use a Git URL with a ref attribute that pins you
   # to a specific version of the modules, such as the following example:
   # source = "git::git@github.com:hashicorp/terraform-aws-consul.git//modules/consul-cluster?ref=v0.0.1"
   source = "./modules/cloud/aws"
+}
+
+module "kubernetes-node" {
+  source = "./modules/kubernetes/node"
+}
+
+module "kubernetes-cluster" {
+  source = "./modules/kubernetes/cluster"
+}
+
+module "kubernetes-common" {
+  source = "./modules/kubernetes/common"
+}
+
+module "cloud-common" {
+  source = "./modules/cloud/common"
 }
