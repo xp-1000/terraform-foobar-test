41470f63f0867977d4e9ad348538429aa678e2cc
Quentin Manfroi <quentin.manfroi@fr.clara.net>
2020-10-23 22:01:48 +0200

    test

--- modules.tf
+++ modules.tf
@@ -1,3 +1,12 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# SIGNALFX MODULES OF DETECTORS
+# This repository contains generic pre-built detectors for SignalFx broken down into multiple terraform modules.
+# It aims to speed up alerting deployment and apply infrastructure as code process to monitoring.
+# It could also be a place to collect, share and improve alerting rules on popular tools and environments through SignalFx.
+# Note that these templates implementation is opinionated and enforce best practices which ensure homogeneity but will 
+# also restrict the usage and capabilities even if it tries to provide as much flexibility as possible.
+# ---------------------------------------------------------------------------------------------------------------------
+
 module "kubernetes-node" {
   source = "./modules/caas/kubernetes/node"
 }
